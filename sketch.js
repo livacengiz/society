0fe5a885fd969bca09b2b0f371c1b9f3384fabc8
Liva Cengiz <ahmetlivacengiz@gmail.com>
2017-06-01 22:03:58 +0300

    some

--- sketch.js
+++ sketch.js
@@ -4,32 +4,27 @@
 // d = right
 //
 
-var palet = {
-  renk1: "214, 157, 14"
-}
-
+var inc = 0;
 var yoff = 1;
 var zoff = 1;
 
-var x = -30; // x position
+var x = -10; // x position
 var y = 0; // y position
-var x1 = 30; // x1 position
+var x1 = 10; // x1 position
 var y1 = 0; // y1 position
 
 var s = 1; // Size
-var speed = 3; // Speed
+var speed = 4; // Speed
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     noStroke();
-    background(255, 90);
+    background(0, 6);
 }
 
 function draw() {
-
-  background(0, 10)
   translate(width/2, height/2)
-
+    var inc = inc + 1;
     var z1 = map(noise(yoff), 0, 1, 0, 40);
     var z2 = map(noise(zoff), 0, 1, 0, 40);
 
@@ -47,24 +42,24 @@ function draw() {
         y1 += speed; // Down
     }
     if (keyIsDown("D".charCodeAt(0))|| keyIsDown(RIGHT_ARROW)) {
-        x += speed; // Right
+        x += speed; // Rightl
       	x1 -= speed; // Left
     }
 
 
+    rotate(TWO_PI+inc/4)
+    for(var i=0; i<90; i++){
 
-    for(var i=0; i<192; i++){
-
-      rotate(TWO_PI/75.0)
+      rotate(cos(50.0))
       smooth();
       // fill(186, 52, 0);
-      fill(palet.renk1)
-      stroke(palet.renk1)
+      // fill(244, 209, 66)
+      stroke(186, 52, 0)
       ellipse(x1+i, y1, s, s);
 
       // fill(173, 130, 12);
-      fill(palet.renk1)
-      stroke(palet.renk1)
+      // fill(244, 209, 66)
+      stroke(186, 52, 0)
       ellipse(x+i, y, s, s);
     }
 
