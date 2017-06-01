f3c2e98df8019a255fd8c54dc88dac117bf52739
Liva Cengiz <ahmetlivacengiz@gmail.com>
2017-06-01 23:47:35 +0300

    implemented random walks

--- sketch.js
+++ sketch.js
@@ -1,20 +1,11 @@
-// w = up
-// a = left
-// s = down
-// d = right
-//
 
-var inc = 0;
-var yoff = 1;
-var zoff = 1;
-
-var x = -10; // x position
+var x = -40; // x position
 var y = 0; // y position
-var x1 = 10; // x1 position
+var x1 = 40; // x1 position
 var y1 = 0; // y1 position
 
-var s = 1; // Size
-var speed = 4; // Speed
+var s = 40; // Size
+var speed = 8; // Speed
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -23,47 +14,46 @@ function setup() {
 }
 
 function draw() {
+  background(0, 10)
+  var r = floor(random(0, 4))
   translate(width/2, height/2)
-    var inc = inc + 1;
-    var z1 = map(noise(yoff), 0, 1, 0, 40);
-    var z2 = map(noise(zoff), 0, 1, 0, 40);
 
-    // Handle keyboard input
-    if (keyIsDown("W".charCodeAt(0)) || keyIsDown(UP_ARROW)) {
-        y -= speed; // Up
-        y1 -= speed; // Up
-    }
-    if (keyIsDown("A".charCodeAt(0)) || keyIsDown(LEFT_ARROW)) {
+    switch (r) {
+      case 3:
+          y -= speed; // Up
+          y1 += speed; // Up
+                break;
+      case 0:
         x -= speed; // Left
-      	x1 += speed; // Right
-    }
-    if (keyIsDown("S".charCodeAt(0)) || keyIsDown(DOWN_ARROW)) {
-        y += speed; // Down
-        y1 += speed; // Down
-    }
-    if (keyIsDown("D".charCodeAt(0))|| keyIsDown(RIGHT_ARROW)) {
+        x1 += speed; // Right
+                break;
+
+      case 1:
+        y += speed ; // Down
+        y1 -= speed ; // Down
+                break;
+
+      case 2:
         x += speed; // Rightl
-      	x1 -= speed; // Left
+        x1 -= speed; // Left
+                break;
+
     }
 
-
-    rotate(TWO_PI+inc/4)
-    for(var i=0; i<90; i++){
-
-      rotate(cos(50.0))
+    for(var i=0; i<9; i++){
+      s = random(4)
+      rotate(radians(30))
       smooth();
       // fill(186, 52, 0);
-      // fill(244, 209, 66)
-      stroke(186, 52, 0)
+      fill(237, 133, 0)
+      stroke(255)
       ellipse(x1+i, y1, s, s);
 
       // fill(173, 130, 12);
-      // fill(244, 209, 66)
-      stroke(186, 52, 0)
+      fill(237, 133, 0)
+      stroke(255)
       ellipse(x+i, y, s, s);
     }
 
-    yoff += 2;
-    zoff += 1;
 
 }
